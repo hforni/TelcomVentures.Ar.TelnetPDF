e38c52149d03fd91c2cf27986fc141edc7bd12b8
Hernan Forni <hernan_forni@antina.com.ar>
2021-01-18 09:02:26 -0300

    first commit

---
+++ ClassParametrosTelnetPDF.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TelnetPDF
+{
+    public class ClassParametrosTelnetPDF
+    {
+        
+        string _urlpath;
+
+        public string Urlpath
+        {
+            get { return _urlpath; }
+            set { _urlpath = value; }
+        }
+        string _FileRoute;
+
+        public string FileRoute
+        {
+            get { return _FileRoute; }
+            set { _FileRoute = value; }
+        }
+        string _urlmensaje;
+
+        public string Urlmensaje
+        {
+            get { return _urlmensaje; }
+            set { _urlmensaje = value; }
+        }
+
+        string _imageroute;
+        public string Imageroute
+        {
+            get { return _imageroute; }
+            set { _imageroute = value; }
+        }
+
+
+        string _CnnStringSqlServerSinPassword;
+        public string CnnStringSqlServerSinPassword
+        {
+            get { return _CnnStringSqlServerSinPassword; }
+            set { _CnnStringSqlServerSinPassword = value; }
+        }
+
+
+        string _PasswordSqlServerEncriptado;
+        public string PasswordSqlServerEncriptado
+        {
+            get { return _PasswordSqlServerEncriptado; }
+            set { _PasswordSqlServerEncriptado = value; }
+        }
+
+
+        string _ServerUniverse;
+        public string ServerUniverse
+        {
+            get { return _ServerUniverse; }
+            set { _ServerUniverse = value; }
+        }
+
+
+        string _UsuarioUniverse;
+        public string UsuarioUniverse
+        {
+            get { return _UsuarioUniverse; }
+            set { _UsuarioUniverse = value; }
+        }
+
+
+        string _PasswordUniverseEncriptado;
+        public string PasswordUniverseEncriptado
+        {
+            get { return _PasswordUniverseEncriptado; }
+            set { _PasswordUniverseEncriptado = value; }
+        }
+
+
+        string _CuentaUniverse;
+        public string CuentaUniverse
+        {
+            get { return _CuentaUniverse; }
+            set { _CuentaUniverse = value; }
+        }
+
+
+        string _Path;
+        public string Path
+        {
+            get { return _Path; }
+            set { _Path = value; }
+        }
+
+
+        string _Path2;
+        public string Path2
+        {
+            get { return _Path2; }
+            set { _Path2 = value; }
+        }
+
+
+        string _Archivo;
+        public string Archivo
+        {
+            get { return _Archivo; }
+            set { _Archivo = value; }
+        }
+
+
+        string _Archivo2;
+        public string Archivo2
+        {
+            get { return _Archivo2; }
+            set { _Archivo2 = value; }
+        }
+
+
+
+        string _IVA;
+        public string IVA
+        {
+            get { return _IVA; }
+            set { _IVA = value; }
+        }
+
+
+        string _CantidadCamposxRegistro;
+        public string CantidadCamposxRegistro
+        {
+            get { return _CantidadCamposxRegistro; }
+            set { _CantidadCamposxRegistro = value; }
+        }
+
+
+        string _LogFile;
+        public string LogFile
+        {
+            get { return _LogFile; }
+            set { _LogFile = value; }
+        }
+
+
+        string _FontRoute;
+        public string FontRoute
+        {
+            get { return _FontRoute; }
+            set { _FontRoute = value; }
+        }
+
+    }
+}
